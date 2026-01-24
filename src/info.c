7ce1ef04f5e040fc27de87dd87df4d2cb11046b7
alx <alx@fastestcode.org>
2026-01-24 14:46:25 +0100

    Updated copyright/version info

--- src/info.c
+++ src/info.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2025 alx@fastestcode.org
+ * Copyright (C) 2023-2026 alx@fastestcode.org
  * This software is distributed under the terms of the MIT/X license.
  * See the included COPYING file for further information.
  */
--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 
 #define APP_VER 1
 #define APP_REV 0
-#define APP_UPD	2
+#define APP_UPD	3
 #ifdef DEBUG
 #define APP_BLD "Debug; Built " __DATE__
 #else
@@ -16,7 +16,7 @@
 
 #define DESCRIPTION_CS "XFile - File manager for Unix/X11"
 
-#define COPYRIGHT_CS "Copyright (C) 2023-2025 by alx@fastestcode.org\n\n" \
+#define COPYRIGHT_CS "Copyright (C) 2023-2026 by alx@fastestcode.org\n\n" \
 	"This program is distributed under the terms of the X/MIT license.\n" \
 	"You may modify and distribute it accordingly.\n" \
 	"See the included COPYING file for detailed information."
